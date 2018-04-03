6088602e1ec9ffcb9afa5329d45030ed3f598f43
WicoWen <is@wico.cc>
2018-04-04 02:46:17 +0800

    add login page

---
+++ front-site/src/components/Login.vue
@@ -0,0 +1,80 @@
+<template>
+
+<div class="login-container">
+    <div class="page-container">
+        <div class="page-content">
+            <div class="content-wrapper">
+
+                <div class="content pb-20">
+                    <form action="index.html">
+                        <div class="panel panel-body login-form">
+                            <div class="text-center">
+                                <h5 class="content-group-lg"></h5>
+                            </div>
+
+                            <div class="form-group has-feedback has-feedback-left">
+                                <input type="text" class="form-control" placeholder="Username">
+                                <div class="form-control-feedback">
+                                    <i class="icon-user text-muted"></i>
+                                </div>
+                            </div>
+
+                            <div class="form-group has-feedback has-feedback-left">
+                                <input type="text" class="form-control" placeholder="Password">
+                                <div class="form-control-feedback">
+                                    <i class="icon-lock2 text-muted"></i>
+                                </div>
+                            </div>
+
+                            <div class="form-group login-options">
+                                <div class="row">
+                                    <div class="col-sm-6">
+                                        <label class="checkbox-inline">
+                                            <input type="checkbox" class="styled" checked="checked">
+                                            Remember
+                                        </label>
+                                    </div>
+
+                                    <div class="col-sm-6 text-right">
+                                        <a href="login_password_recover.html">Forgot password?</a>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <button type="submit" class="btn bg-pink-400 btn-block">Login <i class="icon-arrow-right14 position-right"></i></button>
+                            </div>
+
+                            <div class="content-divider text-muted form-group"><span>or sign in with</span></div>
+                            <ul class="list-inline form-group list-inline-condensed text-center">
+                                <li><a href="#" class="btn border-indigo text-indigo btn-flat btn-icon btn-rounded"><i class="icon-facebook"></i></a></li>
+                                <li><a href="#" class="btn border-info text-info btn-flat btn-icon btn-rounded"><i class="icon-google"></i></a></li>
+                                <li><a href="#" class="btn border-slate-600 text-slate-600 btn-flat btn-icon btn-rounded"><i class="icon-github"></i></a></li>
+                            </ul>
+
+                        </div>
+                    </form>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
+
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data () {
+    return {
+      msg: 'msg'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
