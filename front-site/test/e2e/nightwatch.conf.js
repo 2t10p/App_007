213c42a5bfe9b8c3f31253eb5f3b282c8288992d
WicoWen <is@wico.cc>
2018-04-06 03:17:13 +0800

    setting up test

--- front-site/test/e2e/nightwatch.conf.js
+++ front-site/test/e2e/nightwatch.conf.js
@@ -33,7 +33,7 @@ module.exports = {
         javascriptEnabled: true,
         acceptSslCerts: true,
         chromeOptions: {
-          args: ['headless', 'disable-gpu']
+          args: ['--headless', '--remote-debugging-port=9222', '--disable-gpu=true']
         }
       }
     },
