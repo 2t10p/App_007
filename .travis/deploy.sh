9003751c84f969baad404f8a53373dc83124be9d
WicoWen <is@wico.cc>
2018-04-13 13:50:35 +0800

    trying to add auto deploy

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -8,24 +8,8 @@ if [ $TRAVIS_BRANCH == "master" ] ; then
     # setup ssh agent, git config and remote
     eval "$(ssh-agent -s)"
     ssh-add ~/.ssh/travis_rsa
-    git remote add deploy "deploy@portal.wico.cc:/var/www/portal.wico.cc"
-    git config user.name "Travis CI"
-    git config user.email "travis@wico.cc"
 
-    # commit compressed files and push it to remote
-    rm -f .gitignore
-    cp .travis/deployignore .gitignore
-    git add .
-    git status # debug
-    git commit -m "Deploy compressed files"
-    git push -f deploy HEAD:master
-
-elif [ $TRAVIS_BRANCH == "staging" ] ; then
-
-    # setup ssh agent, git config and remote
-    eval "$(ssh-agent -s)"
-    ssh-add ~/.ssh/travis_rsa
-    git remote add deploy "deploy@portal.wico.cc:/var/www/portal.wico.cc"
+    git remote add deploy "deploy@wico.cc:/var/www/portal.wico.cc/"
     git config user.name "Travis CI"
     git config user.email "travis@wico.cc"
 
