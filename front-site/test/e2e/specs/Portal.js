3025b8bcc6a197404f7f4c51ade7f33801d395fe
WicoWen <is@wico.cc>
2018-04-13 15:35:40 +0800

    add js lint check

--- front-site/test/e2e/specs/Portal.js
+++ front-site/test/e2e/specs/Portal.js
@@ -1,13 +1,12 @@
 module.exports = {
-    'Portal e2e tests': function (browser) {
-      const devServer = browser.globals.devServerURL
-  
-      browser
-        .url(devServer)
-        .waitForElementVisible('#app', 5000)
-        .assert.elementPresent('.portal')
-        .assert.containsText('h1', 'Portal components')
-        .end()
-    }
+  'Portal e2e tests': function (browser) {
+    const devServer = browser.globals.devServerURL
+
+    browser
+      .url(devServer)
+      .waitForElementVisible('#app', 5000)
+      .assert.elementPresent('.portal')
+      .assert.containsText('h1', 'Portal components')
+      .end()
   }
-  
+}
--- front-site/test/e2e/specs/login.js
+++ front-site/test/e2e/specs/login.js
@@ -1,14 +1,13 @@
 module.exports = {
-    'Login e2e tests': function (browser) {
-      const devServer = browser.globals.devServerURL
-  
-      const LoginPage = devServer + '/Login/'
-      browser
-        .url(LoginPage)
-        .waitForElementVisible('#app', 5000)
-        .assert.elementPresent('.login')
-        .assert.containsText('h1', 'Login Page')
-        .end()
-    }
+  'Login e2e tests': function (browser) {
+    const devServer = browser.globals.devServerURL
+
+    const LoginPage = devServer + '/Login/'
+    browser
+      .url(LoginPage)
+      .waitForElementVisible('#app', 5000)
+      .assert.elementPresent('.login')
+      .assert.containsText('h1', 'Login Page')
+      .end()
   }
-  
+}
