251e8694d93063442d08e8849577e8d63c5b8270
WicoWen <is@wico.cc>
2018-04-15 18:40:15 +0800

    fix karma WARN [web-server]404 error

--- front-site/test/unit/karma.conf.js
+++ front-site/test/unit/karma.conf.js
@@ -12,7 +12,7 @@ module.exports = function karmaConfig (config) {
     //    http://karma-runner.github.io/0.13/config/browsers.html
     // 2. add it to the `browsers` array below.
     browsers: ['ChromeHeadless'],
-    frameworks: ['mocha', 'sinon-chai'],
+    frameworks: ['mocha', 'sinon-chai', 'phantomjs-shim'],
     reporters: ['spec', 'coverage'],
     files: ['./index.js'],
     preprocessors: {
@@ -28,6 +28,9 @@ module.exports = function karmaConfig (config) {
         { type: 'lcov', subdir: '.' },
         { type: 'text-summary' }
       ]
+    },
+    proxies: {
+      '/static/': config.root + '/static/'
     }
   })
 }
