d6c9ce8e30ab4111a27a02cd8d1219f21bb308c9
WicoWen <is@wico.cc>
2018-05-02 22:47:56 +0800

    add login page template

--- front-site/src/components/Login.vue
+++ front-site/src/components/Login.vue
@@ -1,7 +1,42 @@
 <template>
-  <div class="login">
-    <h1>{{ msg }}</h1>
+
+<div class="login container">
+  <div class="row">
+    <div class="col col-login mx-auto">
+      <div class="text-center mb-6">
+      </div>
+      <form class="card" action="" method="post">
+        <div class="card-body p-6">
+          <div class="card-title">Login to your account</div>
+          <div class="form-group">
+            <label class="form-label">Email address</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+          </div>
+          <div class="form-group">
+            <label class="form-label">
+              Password
+              <a href="./forgot-password.html" class="float-right small">I forgot password</a>
+            </label>
+            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+          </div>
+          <div class="form-group">
+            <label class="custom-control custom-checkbox">
+              <input type="checkbox" class="custom-control-input" />
+              <span class="custom-control-label">Remember me</span>
+            </label>
+          </div>
+          <div class="form-footer">
+            <button type="submit" class="btn btn-primary btn-block">Sign in</button>
+          </div>
+        </div>
+      </form>
+      <div class="text-center text-muted">
+        Don't have account yet? <a href="./register.html">Sign up</a>
+      </div>
+    </div>
   </div>
+</div>
+
 </template>
 
 <script>
@@ -9,7 +44,6 @@ export default {
   name: 'Login',
   data () {
     return {
-      msg: 'Login Page'
     }
   }
 }
